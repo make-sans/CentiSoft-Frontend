e0270cd1e4ab7db81a9ef17ac554ddcf80f8239a
Nikola Anastasov Velichkov <nikola@velichkov-bg.com>
2019-02-19 12:28:44 +0100

    Refactored code fetching data, refactored code posting data

--- js/developers.js
+++ js/developers.js
@@ -1,55 +1,83 @@
-(function ($) {
-    let hostname = "http://localhost"
-    let port = 57422
-    let secretToken = "VerySecretToken1"
-    let tableCategories = ["ID", "Name", "Email"]
+const HOSTNAME = "http://localhost"
+const PORT = 57422
+const SECRET_TOKEN = "VerySecretToken1"
+const TABLE_CATEGORIES = ["ID", "Name", "Email"]
+
+function createDevTable(categories) {
+    let table = $("<table></table>").addClass("table")
+    let tableHeaderRow = $("<thead><tr></tr></thead>")
+    let tableBody = $("<tbody></tbody")
+
+    categories.forEach(category => {
+        let tableHeader = $("<th scope='col'></th>").text(category)
+        tableHeaderRow.append(tableHeader)
+    })
+
+    table.append(tableBody)
+
+    return table
+}
+
+function addDeveloperToTable(developer, table) {
+    let row = $("<tr></tr>")
+    
+    let id = $(`<th scope="row">${developer.Id}</th>`)
+    let name = $(`<td>${developer.Name}</td>`)
+    let email = $(`<td>${developer.Email}</td>`)
+
+    table.find("tbody").append(row.append(id).append(name).append(email))
+}
+
+function getDevelopers(container) {
+    let table = createDevTable(TABLE_CATEGORIES)
+    container.empty()
 
     $.ajax({
         method: "GET",
-        url: hostname + ":" + port + "/api/developers",
+        url: HOSTNAME + ":" + PORT + "/api/developers",
         dataType: "json",
         headers: {
-            "centisoft_token": secretToken,
+            "centisoft_token": SECRET_TOKEN,
         },
-        "success": (data) => {
-            parseDevelopers(data)
+        success: (developers) => {
+            developers.forEach((dev) => {
+                addDeveloperToTable(dev, table)
+            })
         },
-        "error": (data) => {
+        error: (data) => {
             alert("Something went wrong, please contact developers")
             console.error(data)
         },
     })
 
-    function parseDevelopers(data) {
-        let container = $("#dev-container")
+    container.append(table)
+}
 
-        let table = createDevTable(tableCategories)
+function setupListeners() {
+    $("#add-developer-toggle-button").click(() => {
+        $("#add-developer-form").slideToggle("fast")
+    })
 
-        let tableBody = $("<tbody></tbody>")
-        data.forEach(dev => {
-            let row = $("<tr></tr>")
-
-            let id = $(`<th scope='row'>${dev.Id}</th>`)
-            let name = $(`<td>${dev.Name}</td>`)
-            let email = $(`<td>${dev.Email}</td>`)
-
-            tableBody.append(row.append(id).append(name).append(email))
-        });
-        table.append(tableBody)
-        
-        container.append(table)
-    }
-
-    function createDevTable(categories) {
-        let table = $("<table></table>").addClass("table")
-        let tableHeaderRow = $("<thead><tr></tr></thead>")
-
-        categories.forEach(category => {
-            let tableHeader = $("<th scope='col'></th>").text(category)
-            tableHeaderRow.append(tableHeader)
+    $("#submit-developer").click((event) => {
+        event.preventDefault()
+        $.ajax({
+            method: "POST",
+            url: HOSTNAME + ":" + PORT + "/api/developers",
+            data: $("#add-developer-form").serialize(),
+            headers: {
+                "centisoft_token": SECRET_TOKEN,
+            },
+            success: (newDeveloper) => {
+                addDeveloperToTable(newDeveloper, $("#dev-container").find("table"))
+            },
+            error: (data) => {
+                console.log(data)
+            }
         })
+    })
+}
 
-        return table
-    }
-
-})(jQuery)
+$(document).ready(() => {
+    setupListeners()
+    getDevelopers($("#dev-container"))
+})
